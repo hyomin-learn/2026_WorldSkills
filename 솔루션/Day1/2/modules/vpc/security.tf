88bc29c8e5aa53d29c98ffb726c92548330fcf60
hyomin <ekftjr0330@gmail.com>
2026-08-23 02:35:46 +0900

    feat:skills-terraform

--- 솔루션/Day1/2/modules/vpc/security.tf
+++ 솔루션/Day1/2/modules/vpc/security.tf
@@ -9,17 +9,15 @@ resource "aws_security_group" "vpc_environment" {
   vpc_id      = aws_vpc.main.id
 
   egress {
-    description = "HTTP anyopen"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    description = "HTTPS anyopen"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
